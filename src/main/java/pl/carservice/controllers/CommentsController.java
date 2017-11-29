53ab90972f1ddc326c532d1c0f610343da105127
groszek20 <pea@op.pl>
2017-11-29 22:46:21 +0100

    mapowanie kontrolera

--- src/main/java/pl/carservice/controllers/CommentsController.java
+++ src/main/java/pl/carservice/controllers/CommentsController.java
@@ -10,32 +10,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import pl.carservice.services.IssuesService;
 
 @Controller
-@RequestMapping("/issue")
-public class IssuesController {
-
+@RequestMapping("/comments/**")
+public class CommentsController {
+	
 	private IssuesService issuesService;
-
+	
 	@Autowired
-	public IssuesController(IssuesService issuesService) {
+	public CommentsController(IssuesService issuesService) {
 		super();
 		this.issuesService = issuesService;
 	}
-
+	
 	@RequestMapping("/**")
 	@ResponseBody
-	public String test(Locale locale) {
-		return locale.toString();
-	}
-
-	@RequestMapping("/issue/**")
-	@ResponseBody
-	public String testIssue() {
-		return issuesService.getIssue();
-	}
-	
-	@RequestMapping("/issue/*/test")
-	@ResponseBody
-	public String testIssue2() {
-		return "Mapowanie /issue/*/test";
+	public String test() {
+		return "comment";
 	}
 }
